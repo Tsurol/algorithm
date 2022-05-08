341aae35fe79b350f6595e1d769c48540f217dc9
zzl <zzl@163.com>
2022-05-08 16:09:39 +0800

    delete

--- leetcode_solution.py
+++ leetcode_solution.py
@@ -5,16 +5,6 @@ import error_info as ei
 
 
 class Solution(object):
-    SYMBOL_VALUES = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000,
-    }
-
     def __init__(self):
         pass
 
